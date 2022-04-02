dd35b9049be46cdb374d738bb8f0e014126d0836
Dawid Kubicki <dawid@pluscode.io>
2022-04-02 21:33:43 +0200

    unit converter

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var inputValue = 0.0
+    @FocusState private var amountIsFocused: Bool
+    
     @State private var chosenUnit = "Temperature"
     let units = ["Temperature", "Length", "Time", "Volume"]
+    
+    @State private var lengthUnitInput = "meters"
+    let lengthUnits = ["m", "km", "feet", "yard", "miles"]
+    @State private var lengthUnitOutput = "meters"
+    
+    private var myType: FloatingPointFormatStyle<Double> {
+        return .number
+    }
     
     var body: some View {
         NavigationView {
@@ -30,7 +41,30 @@ struct ContentView: View {
                     if chosenUnit == "Temperature" {
                         Text("Temperature")
                     } else if chosenUnit == "Length" {
-                        Text("Length")
+                        Section {
+                            Picker("Input", selection: $lengthUnitInput) {
+                                ForEach(lengthUnits, id: \.self) {
+                                    Text($0)
+                                }
+                            }.pickerStyle(.segmented)
+                        } header: {
+                            Text("Input units")
+                        }
+                        
+                        Section {
+                            Picker("Output", selection: $lengthUnitOutput) {
+                                ForEach(lengthUnits, id: \.self) {
+                                    Text($0)
+                                }
+                            }.pickerStyle(.segmented)
+                        } header: {
+                            Text("Output units")
+                        }
+                        
+                        TextField("Amount", value: $inputValue, format: myType)
+                                                .keyboardType(.decimalPad)
+                                                .focused($amountIsFocused)
+                        
                     } else if chosenUnit == "Time" {
                         Text("Time")
                     } else if chosenUnit == "Volume" {
@@ -38,9 +72,20 @@ struct ContentView: View {
                     } else {
                         Text("Temperature")
                     }
+                } header: {
+                    Text("Attributes")
                 }
             }
             .navigationTitle("UnitConverter")
+            .toolbar {
+                            ToolbarItemGroup(placement: .keyboard) {
+                                Spacer()
+                                
+                                Button("Done") {
+                                    amountIsFocused = false
+                                }
+                            }
+                        }
         }
     }
 }
