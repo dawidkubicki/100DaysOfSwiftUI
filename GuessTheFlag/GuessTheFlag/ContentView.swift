fac46e0ecad259b36a338516fde2920fd1628818
Dawid Kubicki <dawid@pluscode.io>
2022-04-05 22:57:39 +0200

    minor changes

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button {
+                showAlert()
+            } label: {
+                Label("Sign in with Apple", systemImage: "applelogo")
+                    .padding([.bottom, .top], 14)
+                    .padding([.leading, .trailing], 30)
+                    .background(.black)
+                    .foregroundStyle(.white)
+                    .cornerRadius(20)
+            }
+            .alert("Important message", isPresented: $showingAlert) {
+                Button("OK") {}
+            }
+        }
+    }
+    
+    func showAlert() {
+        showingAlert.toggle()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().preferredColorScheme(.dark)
     }
 }
