194672ee1319a0492be4eb60783e03f5d82d7bfd
Dawid Kubicki <dawid@pluscode.io>
2022-04-05 21:54:10 +0200

    init project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dawid Kubicki on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
